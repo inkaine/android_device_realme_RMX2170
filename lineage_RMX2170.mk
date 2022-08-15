2cd7ca59e3263264d69c5c9e600fb317a3aa683e
Manuel Gaul <57191122+inkaine@users.noreply.github.com>
2022-08-16 01:55:51 +0200

    Create lineage_RMX2170.mk

---
+++ lineage_RMX2170.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/realme/RMX1851/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := lineage_RMX2170
+PRODUCT_DEVICE := RMX2170
+PRODUCT_BRAND := Realme
+PRODUCT_MODEL := RMX2170
+PRODUCT_MANUFACTURER := Realme
+
+#BUILD_FINGERPRINT := "Realme/RMX1851/RMX1851:9/PKQ1.190101.001/1574084019:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+#    PRIVATE_BUILD_DESC="sdm710-user 9 PKQ1.190101.001 eng.root.20191209.210742 release-keys" \
+    PRODUCT_NAME="RMX2170" \
+    TARGET_DEVICE="RMX2170"
+
+PRODUCT_GMS_CLIENTID_BASE := android-realme
